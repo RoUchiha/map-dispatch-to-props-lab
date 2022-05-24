afc642174d9d2bfe70aa064fbde552794ab29824
RoUchiha <swaggsikh@gmail.com>
2022-05-24 01:05:44 -0500

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,8 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state)
+    
   }
 
   render() {
